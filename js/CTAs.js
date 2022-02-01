30103a089a77ce750549cd3255190e41b5e9186b
mohammad-mekawy <mohammad.mekawy92@gmail.com>
2022-02-01 10:45:12 +0200

    Sandeep comments

--- js/CTAs.js
+++ js/CTAs.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
           $(".quote-popup").fadeOut();
           $(".call-overlay").delay(500).fadeOut();
         });
-        $(".btn-call-float").click(function () {
+        $(".btn-call-float,.footer-cta").click(function () {
           $(".call-overlay").fadeIn();
           $(".call-back-popup").delay(500).fadeIn();
         });
@@ -30,6 +30,43 @@ $(document).ready(function () {
           var t = $(this).attr("href");
           $(".tab-content").not(t).css("display", "none").fadeOut(1e3),
             $(t).fadeIn(3e3);
+        });
+        var input1 = document.querySelector("#telephone1");
+        window.intlTelInput(input1, {
+          preferredCountries: ['ae'],
+          separateDialCode: false,
+          nationalMode: false,
+          utilsScript: "assets/js/utils.js",
+        });
+
+        var input2 = document.querySelector("#telephone2");
+        window.intlTelInput(input2, {
+          preferredCountries: ['ae'],
+          separateDialCode: false,
+          nationalMode: false,
+          utilsScript: "assets/js/utils.js",
+        });
+    
+        var input3 = document.querySelector("#telephone3");
+        window.intlTelInput(input3, {
+          preferredCountries: ['ae'],
+          separateDialCode: false,
+          nationalMode: false,
+          utilsScript: "assets/js/utils.js",
+        });
+        var input4 = document.querySelector("#telephone4");
+        window.intlTelInput(input4, {
+          preferredCountries: ['ae'],
+          separateDialCode: false,
+          nationalMode: false,
+          utilsScript: "assets/js/utils.js",
+        });
+        var input5 = document.querySelector("#telephone5");
+        window.intlTelInput(input5, {
+          preferredCountries: ['ae'],
+          separateDialCode: false,
+          nationalMode: false,
+          utilsScript: "assets/js/utils.js",
         });
       });
      
